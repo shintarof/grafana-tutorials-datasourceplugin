9d3845c5df229a8d72b287f7b6659e79e3bf9694
shintaro <you@example.com>
2021-01-26 22:30:52 +0900

    6 Data frames

--- src/DataSource.ts
+++ src/DataSource.ts
@@ -23,14 +23,33 @@ export class DataSource extends DataSourceApi<MyQuery, MyDataSourceOptions> {
 
     // Return a constant for each query.
     const data = options.targets.map(target => {
+      //2.In the map function, use the lodash/defaults package to set default values for query properties that haven’t been set:
       const query = defaults(target, defaultQuery);
-      return new MutableDataFrame({
+
+      //3.Create a data frame with a time field and a number field:
+      const frame = new MutableDataFrame({
         refId: query.refId,
         fields: [
-          { name: 'Time', values: [from, to], type: FieldType.time },
-          { name: 'Value', values: [query.constant, query.constant], type: FieldType.number },
+          { name: 'time', type: FieldType.time },
+          { name: 'value', type: FieldType.number },
         ],
       });
+      
+      //1.Create a couple of helper variables:
+      // duration of the time range, in milliseconds.
+      const duration = to - from;
+      
+      // step determines how close in time (ms) the points will be to each other.
+      const step = duration / 1000;
+
+      //2.Add the values to the data frame:
+      for (let t = 0; t < duration; t += step) {
+        frame.add({ time: from + t, value: Math.sin((2 * Math.PI * t) / duration) });
+      }
+
+      //3.Return the data frame:
+      return frame;
+      
     });
 
     return { data };
